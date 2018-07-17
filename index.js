7c3dada8ece4dbebf1dc53513e508155fa278845
github username <github email address>
2018-07-17 15:31:51 -0400

    First commit complete each

--- index.js
+++ index.js
@@ -4,18 +4,51 @@ fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
+    each: function(collection, iteratee) {
 
+        if (typeof(collection) === "object") {
+
+          for (key in collection) {
+
+            iteratee(collection[key])
+          }
+
+        } else {
+
+          for (el of collection) {
+
+            iteratee(el)
+
+          }
+        }
+      return collection;
     },
 
-    map: function() {
+    map: function(collection, iteratee) {
+      const newCollectionArr = []
+      if (typeof(collection) === "object") {
+
+        for (key in collection) {
+
+          iteratee(collection[key])
+        }
+
+      } else {
+
+        for (el of collection) {
+
+          newCollectionArr.push(iteratee(el))
+
+        }
+        return newCollectionArr;
+      }
 
     },
 
     reduce: function() {
 
     },
-    
+
     functions: function() {
 
     },
